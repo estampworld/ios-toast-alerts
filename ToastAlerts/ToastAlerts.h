c3007e49316fad0b2024d148e1d8805524f97183
Eduardo Irías <eduardo22i@me.com>
2015-08-07 14:53:24 -0700

    Initial Commit

---
+++ ToastAlerts/ToastAlerts.h
@@ -0,0 +1,19 @@
+//
+//  ToastAlerts.h
+//  ToastAlerts
+//
+//  Created by Eduardo Irías on 8/7/15.
+//  Copyright (c) 2015 Estamp World. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToastAlerts.
+FOUNDATION_EXPORT double ToastAlertsVersionNumber;
+
+//! Project version string for ToastAlerts.
+FOUNDATION_EXPORT const unsigned char ToastAlertsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastAlerts/PublicHeader.h>
+
+
